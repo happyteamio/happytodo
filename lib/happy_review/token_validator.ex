ee9320e47fff3a673afc1f51a583334dec22aab5
Maciej Woźniak <iskeld@gmail.com>
2017-04-01 23:28:31 +0200

    Add token validator and tests

---
+++ lib/happy_review/token_validator.ex
@@ -0,0 +1,15 @@
+defmodule HappyReview.TokenValidator do
+  import Plug.Conn
+
+  def init(opts) do
+    Keyword.get(opts, :token, Application.get_env(:happy_review, :token))
+  end
+
+  def call(conn, token) do
+    case conn.body_params do
+      %{"token" => ^token} -> conn
+      %Plug.Conn.Unfetched{} -> raise "Unfetched"
+      %{} -> conn |> send_resp(404, "") |> halt()
+    end
+  end
+end
---
+++ test/token_validator_test.exs
@@ -0,0 +1,62 @@
+defmodule HappyReview.TokenValidatorTest do
+  alias HappyReview.TokenValidator
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  defmodule MyPlug do
+    use Plug.Builder
+
+    @token "gIkuvaNzQIHg97ATvDxqgjtO"
+
+    plug Plug.Parsers, parsers: [:urlencoded]
+    plug TokenValidator, token: @token
+    plug :passthrough
+
+    defp passthrough(conn, _) do
+      Plug.Conn.send_resp(conn, 200, "Passthrough")
+    end
+
+    def token, do: @token
+  end
+
+  defp call(conn), do: MyPlug.call(conn, [])
+
+  test "raises for unfetched body" do
+    conn = conn(:post, "/") |> Map.put(:body_params, %Plug.Conn.Unfetched{aspect: :body_params})
+
+    assert_raise RuntimeError, "Unfetched", fn -> 
+      TokenValidator.call(conn, "")
+    end
+  end
+
+  test "returns 404 for no token" do
+    conn = call(conn(:post, "/"))
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.halted
+  end
+
+  test "returns 404 for invalid token" do
+    conn = conn(:post, "/", "token=th15t0k3n15wr0ng")
+      |> set_content_type()
+      |> call()
+
+    assert conn.state == :sent
+    assert conn.status == 404
+    assert conn.halted
+  end
+
+  test "valid token" do
+    conn = conn(:post, "/", "token=#{MyPlug.token()}")
+      |> set_content_type()
+      |> call()
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Passthrough"
+    refute conn.halted
+  end
+
+  defp set_content_type(conn), do: put_req_header(conn, "content-type", "application/x-www-form-urlencoded")
+end

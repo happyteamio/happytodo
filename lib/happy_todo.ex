aecedabe54f1f18bc48a7006c40724e8bd6549de
Maciej Woźniak <iskeld@gmail.com>
2017-06-12 13:49:33 +0200

    Add Router plug

--- lib/happy_todo.ex
+++ lib/happy_todo.ex
@@ -1,6 +1,4 @@
 defmodule HappyTodo do
-  import Plug.Conn
-  require IEx
   use Plug.Builder
 
   plug Plug.Logger
@@ -8,27 +6,5 @@ defmodule HappyTodo do
   plug HappyTodo.Parser
   plug HappyTodo.TokenValidator
   plug HappyTodo.TeamValidator
-  plug :process
-
-  def request(conn, text) do
-    IO.inspect text
-    conn |> reply("Hasta la vista, baby")
-  end
-
-  def reply(conn, response) do
-    response_struct = HappyTodo.Slack.Response.in_channel(response)
-    json = Poison.encode!(response_struct)
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, json)
-  end
-
-  def process(conn, _opts) do
-    case conn.body_params["command"] do
-      # %{"request"} -> request(conn, conn.body_params["text"])
-      # %{"pick"} -> pick(conn, conn.body_params["text"])
-      # %{"list"} -> list(conn, conn.body_params["text"])
-      %{} -> reply(conn, "Unknown command")
-    end
-  end
+  plug HappyTodo.Router
 end
---
+++ lib/happy_todo/router.ex
@@ -0,0 +1,40 @@
+defmodule HappyTodo.Router do
+  import Plug.Conn
+  import HappyTodo.CommandHandler
+
+  @behaviour Plug
+
+  def init([]), do: []
+
+  def call(conn, _opts), do: handle(conn, conn.assigns[:request])
+
+  defp handle(conn, %HappyTodo.Slack.Request{command: command} = slack_request) do
+    case execute_command(command, slack_request) do
+      {:ok, response} -> conn |> reply(response)
+      :error -> conn |> send_resp(404, "") |> halt
+    end
+  end
+  defp handle(_conn, _request) do
+    raise "Invalid request"
+  end
+  
+  defp execute_command(command, slack_request) do
+    case command do
+      "add" -> {:ok, add(slack_request)}
+      "pick" -> {:ok, pick(slack_request)}
+      "list" -> {:ok, list(slack_request)}
+      _ -> :error
+    end
+  end
+
+  defp reply(conn, response) do
+    response_struct = HappyTodo.Slack.Response.public(response)
+    json = response_struct |> to_slack_response() |> Poison.encode!
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, json)
+  end
+
+  defp to_slack_response({:public, response}), do: HappyTodo.Slack.Response.public(response)
+  defp to_slack_response({:private, response}), do: HappyTodo.Slack.Response.private(response)
+end

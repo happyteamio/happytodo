2a94219eb74e10aaedee6a715b8e7fc8afa62b97
Maciej Woźniak <iskeld@gmail.com>
2017-07-26 21:46:14 +0200

    Update for Elixir 1.5

--- mix.exs
+++ mix.exs
@@ -2,13 +2,15 @@ defmodule HappyTodo.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :happy_todo,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :happy_todo,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -16,8 +18,10 @@ defmodule HappyTodo.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [mod: {HappyTodo.Application, []},
-     extra_applications: [:logger]]
+    [
+      mod: {HappyTodo.Application, []},
+      extra_applications: [:logger]
+    ]
   end
 
   defp aliases do
@@ -34,11 +38,13 @@ defmodule HappyTodo.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:cowboy, "~> 1.1"},
-     {:plug, "~> 1.3"},
-     {:ecto, "~> 2.1"},
-     {:postgrex, "~> 0.13.2"},
-     {:poison, "~> 3.1"},
-     {:distillery, "~> 1.4"}]
+    [
+      {:cowboy, "~> 1.1"},
+      {:plug, "~> 1.4"},
+      {:ecto, "~> 2.1"},
+      {:postgrex, "~> 0.13.2"},
+      {:poison, "~> 3.1"},
+      {:distillery, "~> 1.4"},
+    ]
   end
 end
